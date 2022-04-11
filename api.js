7b39f579c1c5c86a9a4a34b2c6bc432f43f851cc
Alejandro <info@alejandrofm.com>
2022-04-11 14:22:15 +0200

    Creating new elements and filling in them with info from the data source

--- api.js
+++ api.js
@@ -2,27 +2,43 @@ console.log(myData);
 
 let data = myData; 
 
-let tbody = document.getElementById("tbody");
+// let tbody = document.getElementById("tbody");
 
-for (let i = 0; i < data.length; i++) {
-    let tr = document.createElement("tr")
-
-    let td1 = document.createElement("td")
-    let td2 = document.createElement("td")
-    let td3 = document.createElement("td")
-    let td4 = document.createElement("td")
-
-    td1.innerHTML = 1 + i;
-    td2.innerHTML = data[i].views;
-    td3.innerHTML = data[i].downloads;
-    td3.innerHTML = data[i].downloads;
-    td4.innerHTML = data[i].largeImageURL;
-
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    tr.appendChild(td4);
-
-    tbody.appendChild(tr);
+// for (let i = 0; i < data.length; i++) {
     
-}
+//     let tr = document.createElement("tr")
+//     let td1 = document.createElement("td")
+//     let td2 = document.createElement("td")
+//     let td3 = document.createElement("td")
+//     let td4 = document.createElement("td")
+//     td1.innerHTML = 1 + i;
+//     td2.innerHTML = data[i].views;
+//     td3.innerHTML = data[i].downloads;
+//     td3.innerHTML = data[i].downloads;
+//     td4.innerHTML = "<img src='" + data[i].largeImageURL + "' alt='test'/>";
+
+//     tr.appendChild(td1);
+//     tr.appendChild(td2);
+//     tr.appendChild(td3);
+//     tr.appendChild(td4);
+
+//     tbody.appendChild(tr);
+
+//     // ID the element where insert our new content dinamically
+//     let items = document.getElementById("items");
+
+//     let container = document.createElement("div")
+//     container.className = "col-sm-6 col-lg-4 mb-4";
+//     container.innerHTML = "Hola"
+
+//     let card = document.createElement("div")
+//     container.className = "card";
+//     container.innerHTML = "<img src='" + data[i].largeImageURL + "' alt='test' 'width ='100px'/>"
+    
+
+
+//     items.appendChild(container)
+
+
+    
+// }
